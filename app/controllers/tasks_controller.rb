84bb5d1e28eaa083dcaeee286ca006269572445e
fumiya.harada <fumiya.tech@gmail.com>
2020-02-27 01:55:26 +0000

    fix Rails Project

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
     end
 
     def show
-        @tasks = Task.find(params[:id])
+        @task = Task.find(params[:id])
     end
 
     def new
@@ -46,10 +46,11 @@ class TasksController < ApplicationController
         flash[:success] = 'Task は正常に削除されました'
         redirect_to tasks_url
     end
-end
 
-private
 
-def task_params
-    params.require(:task).permit(:content)
+    private
+
+    def task_params
+        params.require(:task).permit(:content)
+    end
 end
